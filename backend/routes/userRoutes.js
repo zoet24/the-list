4d0515b0e0adc196e50a6d09b314efa3429afa5f
Zoe Thexton <zoethexton.dev@gmail.com>
2023-01-14 14:10:33 +0000

    Add login, watchList and favList functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,34 +2,35 @@ const express = require("express");
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Models
 const User = require("../models/User");
 
 router.post(
   "/",
-  asyncHandler(async (req, res) => {
+  asyncHandler(async (req, res, next) => {
     try {
       const { username, password } = req.body;
 
       // Validation
       if (!username || !password) {
         res.status(400);
-        throw new Error("ZARLECK IS DISPLEASED");
+        throw new Error("Missing information");
       }
 
       // Check user doesn't already exist
       const userExists = await User.findOne({ username });
       if (userExists) {
         res.status(400);
-        throw new Error("ZARLECK IS DISPLEASED");
+        throw new Error("User already exists");
       }
 
       // Hash password
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
 
-      //   Create user
+      // Create user
       const user = await User.create({
         username,
         password: hashedPassword,
@@ -37,14 +38,81 @@ router.post(
 
       if (user) {
         res.status(201).json({
-          user,
+          username: user.username,
+          watchList: user.watchList,
+          favList: user.favList,
+          token: generateToken(user._id),
         });
       }
     } catch (error) {
-      res.status(400);
-      throw new Error("ZARLECK IS DISPLEASED");
+      next(error);
     }
   })
 );
+
+router.post(
+  "/login",
+  asyncHandler(async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+
+      //   Check if user exists
+      if (!user) {
+        res.status(404);
+        throw new Error("User not found");
+      }
+
+      //   Check if password exists
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        throw new Error("User not found");
+      }
+
+      res.status(200).json({
+        username: user.username,
+        watchList: user.watchList,
+        favList: user.favList,
+        token: generateToken(user._id),
+      });
+    } catch (error) {
+      next(error);
+    }
+  })
+);
+
+router.put(
+  "/:userId/watchList",
+  asyncHandler(async (req, res, next) => {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+        new: true,
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  })
+);
+
+router.put(
+  "/:userId/favList",
+  asyncHandler(async (req, res, next) => {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.userId, req.body, {
+        new: true,
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  })
+);
+
+const generateToken = (id) => {
+  return jwt.sign({ _id: id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = router;
